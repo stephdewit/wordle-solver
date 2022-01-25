b1017762f6b0ddf206e7cd8f45537f7f827939c5
Stéphane de Wit <contact@stephanedewit.be>
2022-01-26 00:52:03 +0100

    Extract array randomization into its own file

---
+++ randomarrays.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"math/rand"
+	"sort"
+	"time"
+)
+
+var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func getRandomSubArray(words *[]string, howMuch int) []string {
+	if len(*words) <= howMuch {
+		return *words
+	}
+
+	indexes := map[int]bool{}
+	for len(indexes) < howMuch {
+		indexes[rng.Int() % len(*words)] = true
+	}
+
+	var subArray []string
+	for index, _ := range indexes {
+		subArray = append(subArray, (*words)[index])
+	}
+
+	sort.Strings(subArray)
+
+	return subArray
+}

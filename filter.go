f10c0dc29853ad194af6918e365b863c8d5e2d9d
Stéphane de Wit <contact@stephanedewit.be>
2022-01-26 12:28:43 +0100

    Extract method to check for unknown character

--- filter.go
+++ filter.go
@@ -14,18 +14,22 @@ func filterWords(words *[]string, proposal string, result string) []string {
 
 	filtered := []string{}
 	for _, word := range *words {
-		contains := false
-		for _, char := range charsToRemove {
-			if strings.ContainsRune(word, char) {
-				contains = true
-				break
-			}
+		if containsUnknownCharacters(word, proposal, result) {
+			continue
 		}
 
-		if !contains {
-			filtered = append(filtered, word)
-		}
+		filtered = append(filtered, word)
 	}
 
 	return filtered
+}
+
+func containsUnknownCharacters(word string, proposal string, result string) bool {
+	for i, char := range []rune(result) {
+		if char == rune('-') && strings.ContainsRune(word, []rune(proposal)[i]) {
+			return true
+		}
+	}
+
+	return false
 }
